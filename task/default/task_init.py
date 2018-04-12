c45c0ee3743e78c827b888a1883c9cdbd859ed81
luocheng <luocheng@eastmoney.com>
2018-04-12 16:02:03 +0800

    1.0

--- task/default/task_init.py
+++ task/default/task_init.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from config import shadowx_config
-from config.log_config import redis_log
+from server.monitor import LogsFile
 import redis
 
 
 def tasks_clear():
     '''清除backend中redis 数据'''
+    redis_log = LogsFile.get_monitor_logs()
     try:
         re = redis.Redis(host=shadowx_config.redis['host'], port=shadowx_config.redis['port'], db=shadowx_config.redis['db'])
         re.flushdb()
